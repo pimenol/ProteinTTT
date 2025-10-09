d787448ee4de32bc5b926e2cbac0e88364548ba7
pimenol <hella.pimenova@gmail.com>
2025-10-09 17:33:45 +0200

    update projectid

--- run_esm.sh
+++ run_esm.sh
@@ -1,4 +1,4 @@
 source ~/.bashrc
 conda activate esmfold
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-cd /scratch/project/open-32-14/pimenol1/ProteinTTT/ProteinTTT/notebooks
+cd /scratch/project/open-35-8/pimenol1/ProteinTTT/ProteinTTT/notebooks
