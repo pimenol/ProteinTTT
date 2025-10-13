467ac71db24b5c9fb2f3188a5db3c9ce1f4af3e3
pimenol <hella.pimenova@gmail.com>
2025-10-13 17:12:36 +0200

    update running script

--- run_evaluation_bfvd.sh
+++ run_evaluation_bfvd.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #SBATCH --job-name=job
 #SBATCH --account=OPEN-35-8
-#SBATCH --partition=qgpu_free
+#SBATCH --partition=qgpu
 #SBATCH --nodes=1
 #SBATCH --gpus=1
-#SBATCH --time=18:00:00
-#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
 #SBATCH --output=./jobs/fold_%A_%a.out
 #SBATCH --error=./jobs/fold_%A_%a.err
 
 # Activate conda environment
-eval "$(conda shell.bash hook)"
+
+source "/scratch/project/open-32-14/pimenol1/miniconda3/etc/profile.d/conda.sh"
 conda activate esmfold
 
 # Change dir to $WORK (or other location)
-cd "${WORK}/ProteinTTT/ProteinTTT" || exit 1
+# cd "${WORK}/ProteinTTT/ProteinTTT" || exit 1
+cd /scratch/project/open-35-8/pimenol1/ProteinTTT/ProteinTTT || exit 1
 
 CHUNK_START=$1
 CHUNK_END=$2
@@ -22,6 +23,6 @@ CALCULATE_ONLY_TTT=$3
 
 echo "Running job for chunk: $CHUNK_START to $CHUNK_END with calculate_only_ttt set to $CALCULATE_ONLY_TTT"
 # Run script with arguments
-python3 run_bfvd.py --chunk_start "$1" --chunk_end "$2" --calculate_only_ttt "$3"
+python3 run_bfvd.py --chunk_start "$1" --chunk_end "$2" --calculate_only_ttt "$3" --path_to_df "$4"
 
 echo "Job finished successfully."
