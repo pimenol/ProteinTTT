7c1db4bf6a34d8ba16a5c584c9b339de5820ae32
pimenol <hella.pimenova@gmail.com>
2025-10-24 20:31:47 +0200

    Optimize memory by only saving modules from _ttt_get_trainable_modules()

--- proteinttt/base.py
+++ proteinttt/base.py
@@ -153,7 +153,7 @@ class TTTConfig:
                 "msa_soft_labels loss kind can only be used if msa=True"
             )
 
-        if lora_rank > 0 and inject_trainable_lora is None:
+        if self.lora_rank > 0 and inject_trainable_lora is None:
             raise ImportError(
                 "lora_diffusion is not installed. Please install it with "
                 "`pip install git+https://github.com/cloneofsimo/lora.git`."
@@ -680,14 +680,14 @@ class TTTModule(torch.nn.Module, ABC):
         The whole modules rather than parameters are saved to avoid support changing
         modules such as in the case of LoRA.
 
-        TODO: Optimize memory by only saving modules from _ttt_get_trainable_modules()
-
         Returns:
             Dictionary mapping module names to their copied states
         """
         state = {}
+        trainable_modules = set(self._ttt_get_trainable_modules())
         for name, module in self.named_children():
-            state[name] = copy.deepcopy(module)
+            if module in trainable_modules:
+                state[name] = copy.deepcopy(module)
         return state
 
     def _ttt_set_state(self, state: T.Any) -> None:
