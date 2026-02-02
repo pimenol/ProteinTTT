69b31b87ad195aff42039f402dcf0ebea11e8597
pimenol <lisitypi@gmail.com>
2026-02-02 15:32:20 +0100

    Add configuration file, README, and main execution script for ProteinTTT. The config.yaml defines data and output settings, while README.md outlines usage and directory structure. The run_df.py script implements the main processing logic for model predictions and logging.

---
+++ scripts/run_df.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=job
+#SBATCH --account=OPEN-35-8
+#SBATCH --partition=qgpu
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --time=24:00:00
+#SBATCH --output=./jobs/df/df_%A.out
+#SBATCH --error=./jobs/df/df_%A.err
+
+# Activate conda environment
+source "/scratch/project/open-35-8/pimenol1/miniconda3/etc/profile.d/conda.sh"
+conda activate proteinttt2
+
+cd /scratch/project/open-35-8/pimenol1/ProteinTTT/ProteinTTT || exit 1
+export PYTHONPATH="${PYTHONPATH}:/scratch/project/open-35-8/pimenol1/ProteinTTT/ProteinTTT"
+
+python3 ./scripts/run_df.py --config ./scripts/config.yaml
+
+echo "Job finished successfully."
